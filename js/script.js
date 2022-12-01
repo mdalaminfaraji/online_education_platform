b7fc2f318ebb6ec2f71fd880d489c5656cef2882
mdalaminfaraji <alaminice1617@gmail.com>
2022-12-01 21:39:20 +0600

    index complete

--- js/script.js
+++ js/script.js
@@ -1,31 +1,93 @@
-let body=document.body;
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
 
-let profile=document.querySelector('.header .flex .profile');
-document.querySelector('#user-btn').onclick=() =>{
-    profile.classList.toggle('active');
-       searchForm.classList.remove('active');
-}
-let searchForm=document.querySelector('.header .flex .search-form');
-document.querySelector('#search-btn').onclick=() =>{
-    searchForm.classList.toggle('active');
-      profile.classList.remove('active');
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
 }
 
-let sideBar=document.querySelector('.side-bar');
-document.querySelector('#menu-btn').onclick=() =>{
-    sideBar.classList.toggle('active');
-    body.classList.toggle('active')
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
 }
 
-document.querySelector('.side-bar .close-side-bar').onclick=() =>{
-    sideBar.classList.remove('active');
-    body.classList.remove('active')
+if(darkMode === 'enabled'){
+   enableDarkMode();
 }
-window.onscroll=()=>{
-    profile.classList.remove('active');
-      searchForm.classList.remove('active');
-      if(window.innerWidth<1200){
-         sideBar.classList.toggle('active');
-        body.classList.toggle('active')
-      }
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
+
+// let profile=document.querySelector('.header .flex .profile');
+// document.querySelector('#user-btn').onclick=() =>{
+//     profile.classList.toggle('active');
+//        searchForm.classList.remove('active');
+// }
+// let searchForm=document.querySelector('.header .flex .search-form');
+// document.querySelector('#search-btn').onclick=() =>{
+//     searchForm.classList.toggle('active');
+//       profile.classList.remove('active');
+// }
+
+// let sideBar=document.querySelector('.side-bar');
+// document.querySelector('#menu-btn').onclick=() =>{
+//     sideBar.classList.toggle('active');
+//     body.classList.toggle('active')
+// }
+
+// document.querySelector('.side-bar .close-side-bar').onclick=() =>{
+//     sideBar.classList.remove('active');
+//     body.classList.remove('active')
+// }
+// window.onscroll=()=>{
+//     profile.classList.remove('active');
+//       searchForm.classList.remove('active');
+//       if(window.innerWidth<1400){
+//          sideBar.classList.toggle('active');
+//         body.classList.toggle('active')
+//       }
+// }
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   search.classList.remove('active');
+}
+
+let search = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   search.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   sideBar.classList.toggle('active');
+   body.classList.toggle('active');
+}
+
+document.querySelector('#close-btn').onclick = () =>{
+   sideBar.classList.remove('active');
+   body.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   search.classList.remove('active');
+
+   if(window.innerWidth < 1200){
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
 }
